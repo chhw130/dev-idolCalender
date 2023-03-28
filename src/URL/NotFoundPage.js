0e1c3e9aa44e40a17bc56817408369eddfd0e2de
chhw130 <527coco@naver.com>
2023-03-28 19:31:31 +0900

    last commit

--- src/URL/NotFoundPage.js
+++ src/URL/NotFoundPage.js
@@ -4,11 +4,13 @@ import styles from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
   return (
-    <div className={styles.errorPageDiv}>
-      <h1>404 Not Found,,</h1>
-      <p>
-        Not found page, Please go back <Link to="/">Home</Link>
-      </p>
+    <div>
+      <div className={styles.errorPageDiv}>
+        <h1>404 Not Found,,</h1>
+        <p>
+          Not found page, Please go back <Link to="/">Home</Link>
+        </p>
+      </div>
     </div>
   );
 };
